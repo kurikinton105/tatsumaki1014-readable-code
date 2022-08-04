a352ac5df0f240a0378832f8f27c8cd54662113d
tatsumaki1014 <91859537+tatsumaki1014@users.noreply.github.com>
2022-08-04 16:02:43 +0900

    Update task.py
    spec4:ファイルに3つの単語("上手", "一時", "市場")を保存し、出力。

--- task.py
+++ task.py
@@ -1,6 +1,12 @@
-print('上手')
-
 #テキストファイルに書き込み
 dictionary_file = open('dictionary-data.txt', 'w', encoding='UTF-8')
 dictionary_file.write('上手\n')
+dictionary_file.write('一時\n')
+dictionary_file.write('市場\n')
+dictionary_file.close()
+
+#wordsはテキストファイルに保存されている単語
+dictionary_file = open('dictionary-data.txt', 'r', encoding='UTF-8')
+words = dictionary_file.read()
+print(words)
 dictionary_file.close()
