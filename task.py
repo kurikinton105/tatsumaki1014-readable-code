e1ff61730133b883f94dfdd0cf26781d6ac82864
tatsumaki1014 <91859537+tatsumaki1014@users.noreply.github.com>
2022-08-05 13:19:35 +0900

    Update task.py
    spec5:辞書に登録された単語にIDを振り分け

--- task.py
+++ task.py
@@ -1,6 +1,10 @@
 #辞書に単語を登録
 dictionary = ['上手', '一時', '市場']
 
+id_dict = dict()
+
 #変数wordは辞書内に登録されてる単語
-for words in dictionary:
-    print(words)
+for number in range(len(dictionary)):
+    id_dict[dictionary[number]] = number + 1 #見出し語の最初の単語のIDを1に
+
+print(id_dict)
